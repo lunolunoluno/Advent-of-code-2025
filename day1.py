daf5477c5e1ad62b5b6e24ec2bee5032a0306509
lunolunoluno <noellucien2001@gmail.com>
2025-12-04 10:06:04 +0100

    day 1 part. 2 done :D

--- day1.py
+++ day1.py
@@ -1,6 +1,6 @@
 starting_value = 50
 
-if __name__ == "__main__":
+def part1():
     with open("day1_input.txt") as f:
         lines = [line.rstrip() for line in f]
 
@@ -17,4 +17,37 @@ if __name__ == "__main__":
         if value == 0:
             password += 1
     
-    print("password:", password)
+    print("part. 1 password:", password)
+
+
+def part2():
+    with open("day1_input.txt") as f:
+        lines = [line.rstrip() for line in f]
+
+    password = 0
+    value = starting_value
+    for l in lines:
+        prev_value = value
+        steps = int(l[1:])
+
+        if steps > 100:
+            password += steps//100
+            steps %= 100
+
+        if l[0] == 'L':
+            value -= steps
+        if l[0] == 'R':
+            value += steps
+        
+        if (prev_value != 0 and value <= 0) or value >= 100:
+            password += 1
+        value %= 100
+        
+        # print(l, value, password)
+    
+    print("part. 2 password:", password)
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
